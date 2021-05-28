c636cd8c55c733fb80d5c3921f4f5da9f70c395e
Florian Korch <s0568195@htw-berlin.de>
2021-05-28 19:34:37 +0200

    init

---
+++ src/pharmfinder-backend/src/test/java/com/pharm/pharmfinder/PharmFinderApplicationTests.java
@@ -0,0 +1,15 @@
+package com.pharm.pharmfinder;
+
+import net.bytebuddy.implementation.bind.annotation.IgnoreForBinding;
+import org.junit.Ignore;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PharmFinderApplicationTests {
+    @Ignore
+    @Test
+    void contextLoads() {
+    }
+
+}
