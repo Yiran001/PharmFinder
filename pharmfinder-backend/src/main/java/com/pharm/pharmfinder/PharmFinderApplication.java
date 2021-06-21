f821642aba0e169691787900e7fb18424b93112f
Florian Korch <s0568195@htw-berlin.de>
2021-06-21 16:22:40 +0200

    initial setup2

---
+++ pharmfinder-backend/src/main/java/com/pharm/pharmfinder/PharmFinderApplication.java
@@ -0,0 +1,13 @@
+package com.pharm.pharmfinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PharmFinderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PharmFinderApplication.class, args);
+    }
+
+}
