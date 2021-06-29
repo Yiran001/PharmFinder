00ae19d9fcf49e7a7faa92144b83634c4b25b7f0
LuckyLuke16 <83580682+LuckyLuke16@users.noreply.github.com>
2021-06-29 22:59:40 +0200

    -nav-Bar und footer mit Unterseiten Über+Datenschutzerklärung -homepage

--- pharmfinder-frontend/src/app/app-routing.module.ts
+++ pharmfinder-frontend/src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {FrontPageComponent} from "./front-page/front-page.component";
+import {AboutPageComponent} from "./about-page/about-page.component";
+import {DatenschutzerklaerungComponent} from "./datenschutzerklaerung/datenschutzerklaerung.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'home', component:FrontPageComponent},
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'about', component:AboutPageComponent },
+  { path: 'datenschutzerklaerung', component:DatenschutzerklaerungComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- pharmfinder-frontend/src/app/app.component.ts
+++ pharmfinder-frontend/src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import {AfterViewInit, Component, ElementRef} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'myapp';
+export class AppComponent implements AfterViewInit{
+  title = 'Apothekenfinder';
+  page1 = 'Suchen';
+  page2 = 'Bestand';
+
+  constructor(private elementRef: ElementRef){
+
+
+  }
+  ngAfterViewInit(){
+    this.elementRef.nativeElement.ownerDocument.body.style.backgroundColor = '';
+  }
+
 }
