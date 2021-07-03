a66845e67cb1d7d8582730fd7c3274680fee8eac
Yiran001 <korchflorian@gmail.com>
2021-07-03 19:24:18 +0200

    Revert "WB-18, WB-20 setup database and implementation of access"
    This reverts commit cf9667cc4de9edd33b0acdae720d6fe32afa6354.

--- pharmfinder-backend/src/main/java/com/pharm/pharmfinder/controller/repositories/PharmFinderPharmacyRepository.java
+++
@@ -1,9 +0,0 @@
-package com.pharm.pharmfinder.controller.repositories;
-
-
-import com.pharm.pharmfinder.model.PharmFinderPharmacy;
-import org.springframework.data.repository.CrudRepository;
-
-public interface PharmFinderPharmacyRepository extends CrudRepository<PharmFinderPharmacy,Integer> {
-
-}
--- pharmfinder-backend/src/main/java/com/pharm/pharmfinder/controller/repositories/PharmacyUserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.pharm.pharmfinder.controller.repositories;
-
-import com.pharm.pharmfinder.model.PharmacyUser;
-import org.springframework.data.repository.CrudRepository;
-
-public interface PharmacyUserRepository extends CrudRepository<PharmacyUser, Integer> {
-
-}
-
