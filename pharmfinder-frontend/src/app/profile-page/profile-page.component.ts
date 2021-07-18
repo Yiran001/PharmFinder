9cf88596847c44544eecec5536be8ea2381ade85
Yiran001 <korchflorian@gmail.com>
2021-07-18 20:50:41 +0200

    profile page only layout left

--- pharmfinder-frontend/src/app/profile-page/profile-page.component.ts
+++ pharmfinder-frontend/src/app/profile-page/profile-page.component.ts
@@ -1,20 +1,61 @@
-import {Component, Injectable, OnInit} from '@angular/core';
-import {ProfileService, User} from "../services/profile.service";
+import {Component, OnInit} from '@angular/core';
+import {Address, ProfileService, User} from "../services/profile.service";
 import {Router} from "@angular/router";
-import { Observable } from 'rxjs';
-import {InjectableDecoratorHandler} from "@angular/compiler-cli/src/ngtsc/annotations";
 
 @Component({
   selector: 'app-profile-page',
   templateUrl: './profile-page.component.html',
+  providers: [ProfileService],
   styleUrls: ['./profile-page.component.css']
 })
 export class ProfilePageComponent implements OnInit {
-  username: Observable<User> | undefined;
-  constructor(private profileService: ProfileService,private router: Router) { }
 
-  ngOnInit(): void {
-    this.profileService.getUsers();
+  users: Array<User> = [];
+  user: User | undefined;
+
+  username = "";
+  email = "";
+  isPharmacist = "";
+  addresses: Address[] = [];
+  address: Address | undefined;
+  street = "";
+  houseNumber = "";
+  postcode = "";
+
+  constructor(private profileService: ProfileService, private router: Router) {
   }
 
+  ngOnInit(): void {
+    this.getUsers()
+    this.getAddresses()
+
+  }
+
+  async getUsers(): Promise<void> {
+    this.users = await this.profileService.getUsers().toPromise();
+    console.log(this.users);
+    this.user = this.users[0];
+    this.username = this.user.username;
+    this.email = this.user.email;
+    if (this.user.pharmacist) {
+      this.isPharmacist = "Ja";
+    } else {
+      this.isPharmacist = "Nein";
+    }
+
+
+  }
+
+  async getAddresses(): Promise<void> {
+
+    this.addresses = await this.profileService.getAddresses().toPromise();
+    console.log(this.addresses)
+    this.addresses.forEach(address => {
+      if (address.addressUsers[0].username == this.username) {
+        this.street = address.street;
+        this.houseNumber = address.houseNumber;
+        this.postcode = address.postcode;
+      }
+    })
+  }
 }
--- pharmfinder-frontend/src/app/services/profile.service.ts
+++ pharmfinder-frontend/src/app/services/profile.service.ts
@@ -1,13 +1,20 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
 import {HttpClient} from "@angular/common/http";
-import { Observable, throwError } from 'rxjs';
-import { catchError, retry } from 'rxjs/operators';
+import {tap} from 'rxjs/operators';
 
 export interface User {
-  userID: number;
+  userID: any;
   username: string;
   email: string;
   pharmacist: boolean;
+}
+
+export interface Address {
+  addressID: any;
+  addressUsers: User[];
+  street: string;
+  houseNumber: string;
+  postcode: string;
 }
 
 @Injectable({
@@ -15,9 +22,16 @@ export interface User {
 })
 export class ProfileService {
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {
+  }
 
   getUsers() {
-    return this.http.get('http://localhost:8080/users/index').subscribe(responseData => console.log(responseData));
+    return this.http.get<User[]>('http://localhost:8080/users/index').pipe(
+      tap(_ => console.log(_)));
+  }
+
+  getAddresses() {
+    return this.http.get<Address[]>('http://localhost:8080/addresses/index').pipe(
+      tap(_ => console.log(_)));
   }
 }
