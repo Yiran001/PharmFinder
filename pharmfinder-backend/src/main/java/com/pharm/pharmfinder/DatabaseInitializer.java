742e102942ce61fa30b564c4ec7c6b07d31f260f
dergil <cpconrad@posteo.de>
2021-09-08 14:06:33 +0200

    test

--- pharmfinder-backend/src/main/java/com/pharm/pharmfinder/DatabaseInitializer.java
+++ pharmfinder-backend/src/main/java/com/pharm/pharmfinder/DatabaseInitializer.java
@@ -12,7 +12,7 @@ import org.springframework.stereotype.Component;
 
 import java.util.List;
 
-
+// test comment
 @Component
 public class DatabaseInitializer implements ApplicationListener<ApplicationReadyEvent> {
     @Autowired
